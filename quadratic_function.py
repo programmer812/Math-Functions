98cd395435da8b802f09e94b2b1735afbb154709
programmer812 <anishkhu264@gmail.com>
2020-04-26 11:35:46 -0600

    Removed all print statements
    Replaced it with return statements for returning values
    Replaced it with raise statements for returning errors

--- quadratic_function.py
+++ quadratic_function.py
@@ -1,19 +1,21 @@
 def quadratic_function(a, b, c):
+    assert type(a) == int, "Please input integer numbers"
+    assert type(b) == int, "Please input integer numbers"
+    assert type(c) == int, "Please input integer numbers"
     if a == 0:
-        print("Denominator cannot be 0")
+        raise ZeroDivisionError("Cannot divide by 0")
     else:
         if (b ** 2) - (4 * a * c) < 0:
-            print("Cannot square root a negative number")
+            raise ValueError("Cannot square root a negative number")
         elif (b ** 2) - (4 * a * c) == 0:
-            print("There is one real root")
             x = (-b + ((b ** 2) - (4 * a * c)) ** 0.5) / 2 * a
-            print(x)
+            return x
         else:
             print("There are two real roots")
             x = (-b + ((b ** 2) - (4 * a * c)) ** 0.5) / 2 * a
             y = (-b - ((b ** 2) - (4 * a * c)) ** 0.5) / 2 * a
-            print(x,y)
+            return x,y
 
-quadratic_function(1, 4, 4)
-quadratic_function(0, 4, 4)
-quadratic_function(10, 1, 10)
+print(quadratic_function(1, 4, 4))
+print(quadratic_function(0, 4, 4))
+print(quadratic_function(10, 1, 10))
