6d3a62990bd774dff5c5fd06d9c3ca68b1be8ac8
programmer812 <anishkhu264@gmail.com>
2020-04-29 23:27:53 -0600

    Created a new file: fib.py

---
+++ fib.py
@@ -0,0 +1,15 @@
+# Fibonacci Sequence
+
+# 1,1,2,3,5,8,13,21,34,55...
+
+def fib(n):
+    if n < 1:
+        return "Invalid Input"
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(5)) # 5
+print(fib(10)) # 55
+print(fib(-3)) # "Invalid Input" 
