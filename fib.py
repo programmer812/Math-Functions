3158e70674e8df8030e0e598db0a931e8e120679
programmer812 <anishkhu264@gmail.com>
2020-04-29 23:34:03 -0600

    Modified a file: fib.py

--- fib.py
+++ fib.py
@@ -3,6 +3,9 @@
 # 1,1,2,3,5,8,13,21,34,55...
 
 def fib(n):
+    '''Will check the nth number in the fibonacci sequence'''
+    
+    assert type(n) == int, "'n' must be an integer"
     if n < 1:
         return "Invalid Input"
     elif n == 1 or n == 2:
@@ -12,4 +15,5 @@ def fib(n):
 
 print(fib(5)) # 5
 print(fib(10)) # 55
-print(fib(-3)) # "Invalid Input" 
+print(fib(-3)) # "Invalid Input"
+print(fib("1")) # AssertionError
